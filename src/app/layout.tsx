d14377b5bdfa18a9d56706b68d9a40db5b63e446
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2024-03-01 18:41:07 +0530

    vfdcd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
+//@ts-nocheck
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Providers } from "./providers";
+import favicon from "../assets/Img/me.jpg"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Gaurav Sunthwal",
-  description: "Hello, I'm Gaurav Sunthwal, a software engineer and web developer completing my BTech at MIT WPU, Pune. Proficient in Next.js, React.js, JavaScript, Python, HTML, CSS, Chakra UI/Tailwind CSS, Node.js, MongoDB, and more, I specialize in crafting dynamic and user-friendly web applications. With a passion for innovation and a commitment to delivering high-quality solutions, I thrive on the challenges of modern web development. Outside of coding, I'm active in the developer community and dedicated to leveraging technology for positive change.",
+  description:
+    "Hello, I'm Gaurav Sunthwal, a software engineer and web developer completing my BTech at MIT WPU, Pune. Proficient in Next.js, React.js, JavaScript, Python, HTML, CSS, Chakra UI/Tailwind CSS, Node.js, MongoDB, and more, I specialize in crafting dynamic and user-friendly web applications. With a passion for innovation and a commitment to delivering high-quality solutions, I thrive on the challenges of modern web development. Outside of coding, I'm active in the developer community and dedicated to leveraging technology for positive change.",
 };
 
 export default function RootLayout({
@@ -19,6 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* <link
+          rel="icon"
+          href={favicon}
+          type="image/<generated>"
+          sizes="<generated>"
+        /> */}
         <meta
           name="google-site-verification"
           content="brnrAvH6YNLyRPlnUkA-3zemJp4es-Q9WvGhuEnt-no"
